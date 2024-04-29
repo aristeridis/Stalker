27ab8e244bdc264b94b2982f567341f608234b85
alexandrosaristeridis <aristeridis@hotmail.gr>
2024-04-29 18:04:24 +0300

    login and register screens style

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Form } from "antd";
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom'
 
-function Login () {
+function Register() {
+  const onFinish = (values) => {
+    console.log("Success: ", values);
+  }
   return (
-    <div>Login </div>
+    <div className='flex justify-center h-screen items-center bg-primary '>
+      <div className='card p-3 w-400 '>
+        <h1 className="text-xl mb-1 ">
+          STALKER -  LOGIN
+        </h1>
+        <Form layout='vertical' className='mt-1' onFinish={onFinish}>
+          <Form.Item label='Email' name='email' rules={[{ required: true, message: "Please input your email" }]}>
+            <input type='email' /></Form.Item>
+          <Form.Item label='Password' name='password' rules={[{ required: true, message: "Please input your password" }]}>
+            <input type='password' /></Form.Item>
+          <div className='flex flex-column mt-2 gap-1'>
+            <Button fullWidth title='LOGIN' type='submit' /></div>
+          <Link to="/register" className='text-primary'>Αν δεν έχετε ήδη λογιαριασμό? Register</Link>
+
+
+        </Form>
+      </div>
+
+    </div>
   )
 }
 
-export default Login 
+export default Register
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import { Button } from "antd";
+import { Form } from "antd";
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom'
 
 function Register() {
+  const onFinish = (values) => {
+    console.log("Success: ", values);
+  }
   return (
-    <div className='flex justify-center h-screen items-center'>
-      <Button type="primary">Primary Button</Button>
+    <div className='flex justify-center h-screen items-center bg-primary '>
+      <div className='card p-3 w-400 '>
+        <h1 className="text-xl mb-1 ">
+          STALKER -  REGISTER
+        </h1>
+        <Form layout='vertical' className='mt-1' onFinish={onFinish}>
+          <Form.Item label='Name' name='name' rules={[{ required: true, message: "Please input your name" }]}>
+            <input type='text' /></Form.Item>
+          <Form.Item label='Email' name='email' rules={[{ required: true, message: "Please input your email" }]}>
+            <input type='email' /></Form.Item>
+          <Form.Item label='Password' name='password' rules={[{ required: true, message: "Please input your password" }]}>
+            <input type='password' /></Form.Item>
+          <div className='flex flex-column mt-2 gap-1'>
+            <Button fullWidth title='REGISTER' type='submit' /></div>
+          <Link to="/login" className='text-primary'>Έχετε ήδη λογιαριασμό? Login</Link>
+
+
+        </Form>
+      </div>
 
     </div>
   )
 }
-  
+
 export default Register
